1f384383e0af302efacff9e9d8dd5b3ca6fca40e
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-06 12:17:05 +0100

    upload of entry files

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  messageService: MessagesService
+
+  constructor() {
+    // DONT DO THIS ON REAL APP
+    // USE DEPENDENCY INJECTION
+    this.messageService = new MessagesService()
+  }
+
+  @Get()
+  listMessages() {
+    return this.messageService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messageService.create(body.content)
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return this.messageService.findOne(id);
+  }
+}
